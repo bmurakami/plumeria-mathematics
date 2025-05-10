0816e4a5fdef30ea7137936cfc2a3cb6eed8852c
Brandon Murakami <bmurakami@berkeley.edu>
2025-05-09 20:59:21 -0700

    Add Scalar

---
+++ Sources/Tensors/Scalar.swift
@@ -0,0 +1,8 @@
+public protocol Scalar {
+    static func + (lhs: Self, rhs: Self) -> Self
+    static func - (lhs: Self, rhs: Self) -> Self
+    static func * (lhs: Self, rhs: Self) -> Self
+    static func / (lhs: Self, rhs: Self) -> Self
+}
+
+extension Double: Scalar {}
