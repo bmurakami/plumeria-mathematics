64d3f6c3e99451290693af2f1a2b9ae8ca25fd46
Brandon Murakami <bmurakami@berkeley.edu>
2025-05-09 21:20:32 -0700

    Add Vector tests

--- Sources/Tensors/Vector.swift
+++ Sources/Tensors/Vector.swift
@@ -1,5 +1,6 @@
 public protocol Vector {
     associatedtype Value
+    
     var count: Int { get }
     subscript(i: Int) -> Value { get set }
 }
--- Tests/TensorsTests/VectorTests.swift
+++ Tests/TensorsTests/VectorTests.swift
@@ -3,17 +3,9 @@ import Testing
 
 @Test
 func RealDenseVector_initializerWithValues() throws {
-    let v = try Vector([1.2, 3.4, 5.6])
-    #expect(v.size == 3)
+    let v = RealDenseVector([1.2, 3.4, 5.6])
+    #expect(v.count == 3)
     #expect(v[0] == 1.2)
     #expect(v[1] == 3.4)
     #expect(v[2] == 5.6)
-}
-
-@Test
-func RealDenseVector_initializerValidation() throws {
-    let emptyVector: [Double] = []
-    #expect(throws: MatrixError.self) {
-        try Vector(emptyVector)
-    }
 }
