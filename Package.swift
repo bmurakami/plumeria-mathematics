e921146a891fe02a390314b922073a4485d6e19f
Brandon Murakami <bmurakami@berkeley.edu>
2025-05-10 21:15:54 -0700

    Switch from dynamic to static linking

--- Package.swift
+++ Package.swift
@@ -16,13 +16,12 @@ let package = Package(
             name: "LinearSolvers",
             dependencies: ["Tensors","COpenBLAS"],
             cSettings: [.define("ACCELERATE_NEW_LAPACK")],
-            linkerSettings: [.unsafeFlags(["-L/Users/murakami1/.local/spack/opt/spack/darwin-m1/openblas-0.3.29-urtlqs4wcu3shs6ryfazhsk7rknmjklu"])]
+            linkerSettings: [.unsafeFlags(["/Users/murakami1/.local/spack/opt/spack/darwin-m1/openblas-0.3.29-urtlqs4wcu3shs6ryfazhsk7rknmjklu/lib/libopenblas.a"])]
         ),
         .testTarget(name: "TensorsTests", dependencies: ["Tensors"]),
         .testTarget(
             name: "LinearSolversTests",
-            dependencies: ["LinearSolvers", "COpenBLAS"],
-            linkerSettings: [.unsafeFlags(["-L/Users/murakami1/.local/spack/opt/spack/darwin-m1/openblas-0.3.29-urtlqs4wcu3shs6ryfazhsk7rknmjklu"])]
+            dependencies: ["LinearSolvers", "COpenBLAS"]
         ),
     ]
 )
