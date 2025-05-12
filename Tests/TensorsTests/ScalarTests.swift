8486fcb7e407288dd638ebe4c33a09fdb1088541
Brandon Murakami <bmurakami@berkeley.edu>
2025-05-12 16:32:53 -0700

    Add more scalar tests

--- Tests/TensorsTests/ScalarTests.swift
+++ Tests/TensorsTests/ScalarTests.swift
@@ -1,7 +1,7 @@
 import Testing
 @testable import Tensors
 
-@Test func approximatelyEquals_correctness() {
+@Test func approximatelyEquals_defaultTolerance() {
     let testCases = [
         (1e-16, true),
         (1e-15, true),
@@ -13,5 +13,20 @@ import Testing
         let epsilon = testCase.0
         let y = 1.0 + epsilon
         #expect(x.approximatelyEquals(y) == testCase.1)
+    }
+}
+
+@Test func approximatelyEquals_tolerance() {
+    let testCases = [
+        (1e-5, true),
+        (1e-4, true),
+        (1e-3, false),
+        (1e-2, false)
+    ]
+    let x = 1.0
+    for testCase in testCases {
+        let epsilon = testCase.0
+        let y = 1.0 + epsilon
+        #expect(x.approximatelyEquals(y, tolerance: 1e-4) == testCase.1)
     }
 }
