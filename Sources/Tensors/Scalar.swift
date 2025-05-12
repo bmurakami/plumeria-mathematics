82b726a714a13212d13967069d5acabf23713e20
Brandon Murakami <bmurakami@berkeley.edu>
2025-05-12 13:52:47 -0700

    Remove unused Scalar protocol

--- Sources/Tensors/Scalar.swift
+++ Sources/Tensors/Scalar.swift
@@ -1,8 +1,1 @@
-public protocol Scalar: Numeric {
-    static func + (lhs: Self, rhs: Self) -> Self
-    static func - (lhs: Self, rhs: Self) -> Self
-    static func * (lhs: Self, rhs: Self) -> Self
-    static func / (lhs: Self, rhs: Self) -> Self
-}
 
-extension Double: Scalar {}
