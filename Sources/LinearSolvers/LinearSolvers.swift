e9e0339b016631da960914bd9b58207a50292659
Brandon Murakami <bmurakami@berkeley.edu>
2025-06-21 20:51:58 -0700

    Remove unused file

--- Sources/LinearSolvers/LinearSolvers.swift
+++
@@ -1,9 +0,0 @@
-//import Tensors
-//
-//public typealias LinearSolver = (any Matrix, any Vector) throws -> any Vector
-//
-//enum LinearSolverError: Error {
-//    case malformedProblem(message: String)
-//    case singularMatrix(message: String)
-//    case errorFromImplementation(code: Int)
-//}
