4829f293424033351fcd93f65b08cb1af3efaf2c
Brandon Murakami <bmurakami@berkeley.edu>
2025-07-13 16:49:55 -0700

    Fix darwin-m1 build

--- BuildScripts/install-pluscilib-darwin-m1.sh
+++ BuildScripts/install-pluscilib-darwin-m1.sh
@@ -4,5 +4,7 @@ set -e
 curl -L "https://github.com/bmurakami/plumeria-scientific-libraries/releases/download/test-release/plumeria-libraries-darwin-m1.zip" -o openblas.zip
 unzip -o openblas.zip -d Sources/COpenBLAS
 rm openblas.zip
-ln -s Sources/COpenBLAS/include/openblas/cblas.h Sources/COpenBLAS/include/cblas.h
-ln -s Sources/COpenBLAS/include/openblas/lapack.h Sources/COpenBLAS/include/lapack.h
+cd Sources/COpenBLAS/include
+cp -r openblas/* .
+rm -rf openblas
+cd -
