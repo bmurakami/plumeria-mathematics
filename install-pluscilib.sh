3430f23d8c1627065f61606ca2292715a7feb773
Brandon Murakami <bmurakami@berkeley.edu>
2025-07-13 15:49:26 -0700

    Generalize install script

--- install-pluscilib.sh
+++ install-pluscilib.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl -L "https://github.com/bmurakami/plumeria-scientific-libraries/releases/download/test-release/plumeria-libraries-darwin-m1.zip" -o openblas.zip
+architecture=${1:-"darwin-m1"}
+curl -L "https://github.com/bmurakami/plumeria-scientific-libraries/releases/download/test-release/plumeria-libraries-${architecture}.zip" -o openblas.zip
 unzip -o openblas.zip -d Sources/COpenBLAS
 rm openblas.zip
