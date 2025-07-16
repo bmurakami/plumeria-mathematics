420fad46368a66e5031dadef82250b2d67bfcbf2
Brandon Murakami <bmurakami@berkeley.edu>
2025-07-15 20:54:35 -0700

    Rename pluscilib artifacts

--- BuildScripts/install-pluscilib-darwin-m1.sh
+++ BuildScripts/install-pluscilib-darwin-m1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-curl -L "https://github.com/bmurakami/plumeria-scientific-libraries/releases/download/test-release/plumeria-libraries-darwin-m1.zip" -o openblas.zip
+curl -L "https://github.com/bmurakami/plumeria-scientific-libraries/releases/download/test-release/pluscilib-darwin-m1.zip" -o openblas.zip
 unzip -o openblas.zip -d Sources/COpenBLAS
 rm openblas.zip
 cd Sources/COpenBLAS/include
