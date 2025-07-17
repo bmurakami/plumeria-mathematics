01b4e9296c91b9d6ce1178e1c02fa76e547f7b58
Brandon Murakami <bmurakami@berkeley.edu>
2025-07-16 19:50:24 -0700

    Fix filename

--- BuildScripts/install-pluscilib-linux-x86_64.sh
+++ BuildScripts/install-pluscilib-linux-x86_64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-curl -L "https://github.com/bmurakami/plumeria-scientific-libraries/releases/download/test-release/plumeria-libraries-linux-x86_64.zip" -o openblas.zip
+curl -L "https://github.com/bmurakami/plumeria-scientific-libraries/releases/download/test-release/pluscilib-linux-x86_64.zip" -o openblas.zip
 unzip -o openblas.zip -d Sources/COpenBLAS
 rm openblas.zip
