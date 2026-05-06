39fadd492269fac32f4ca271c797fcdde7d91140
Brandon Murakami <bmurakami@berkeley.edu>
2026-05-05 22:12:51 -0700

    Update for new dependency tagging

--- BuildScripts/install-pluscilib-linux-x86_64.sh
+++ BuildScripts/install-pluscilib-linux-x86_64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-curl -L "https://github.com/bmurakami/plumeria-scientific-libraries/releases/download/test-release/pluscilib-linux-x86_64.zip" -o openblas.zip
-unzip -o openblas.zip -d Sources/COpenBLAS
-rm openblas.zip
+curl -L "https://github.com/bmurakami/plumeria-scientific-libraries/releases/download/0.1-latest/pluscilib-linux-x86_64.tar.gz" -o openblas.tar.gz
+tar -xzf openblas.tar.gz -C Sources/COpenBLAS
+rm openblas.tar.gz
