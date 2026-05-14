92414ba5b91ee700d5c8e5ccb13f10e6b22a078b
Brandon Murakami <bmurakami@berkeley.edu>
2026-05-13 19:53:04 -0700

    Add complex scalar notation

--- Sources/Tensors/PluScalar.swift
+++ Sources/Tensors/PluScalar.swift
@@ -5,21 +5,39 @@ public typealias Complex = Numerics.Complex<Double>
 
 public protocol PluScalar: ElementaryFunctions & PluTensor & Numeric {
     static func / (lhs: Self, rhs: Self) -> Self
-    func round() -> Self
+    func round(precision: Int) -> Self
+}
+
+public protocol ComplexScalar: PluScalar {
+    var star: Self { get }
+    var dagger: Self { get }
+    var mod: Magnitude { get }
+    var arg: Magnitude { get }
 }
 
 extension Double: PluScalar {
     // MARK: - PluScalar conformance
-    public func round() -> Double {
-        let precision = 14
+    public func round(precision: Int = 14) -> Double {
         let multiplier = Foundation.pow(10.0, Double(precision))
         return (self * multiplier).rounded() / multiplier
     }
 }
 
-extension Complex: PluTensor, PluScalar {
+extension Complex: PluTensor, PluScalar, ComplexScalar {
+    // MARK: - ComplexScalar conformance
+    public var star: Complex { conjugate }
+    public var dagger: Complex { star }
+    public var mod: Double { length }
+    public var arg: Double { phase }
+
     // MARK: - PluScalar conformance
-    public func round() -> Complex {
-        return Complex(real.round(), imaginary.round())
+    public func round(precision: Int = 14) -> Complex {
+        return Complex(real.round(precision: precision), imaginary.round(precision: precision))
+    }
+}
+
+extension PluScalar {
+    public func round() -> Self {
+        round(precision: 14)
     }
 }
--- Tests/TensorsTests/ScalarTests.swift
+++ Tests/TensorsTests/ScalarTests.swift
@@ -17,4 +17,21 @@ import Testing
     #expect(a+b == Complex(4.6, 3.3))
     #expect(a+b == b+a)
     #expect((a-b).isApproximatelyEqual(to: Complex(-2.2, -7.9)))
+    #expect((a*b).isApproximatelyEqual(to: Complex(16.96, -1.1)))
+    #expect((a/b).isApproximatelyEqual(to: Complex(-0.20503261882572227, -0.3387698042870456)))
+}
+
+@Test func Complex_plumeriaScalarProperties() {
+    let z = Complex(3.0, 4.0)
+
+    #expect(z.star == Complex(3.0, -4.0))
+    #expect(z.dagger == z.star)
+    #expect(z.mod == 5.0)
+    #expect(z.arg.isApproximatelyEqual(to: 0.9272952180016122, relativeTolerance: 1e-15))
+}
+
+@Test func PluScalar_roundsWithPrecision() {
+    #expect(1.23456.round() == 1.23456)
+    #expect(1.23456.round(precision: 2) == 1.23)
+    #expect(Complex(1.23456, -2.34567).round(precision: 2) == Complex(1.23, -2.35))
 }
